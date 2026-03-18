da04d2c1f09b366ec1c55201641283722f06b812
niminypiminy <liam.ruaidhri@proton.me>
2026-03-18 13:10:32 +0700

    update

--- src/ws/config.rs
+++
@@ -1,4 +0,0 @@
-pub const RELAY_WS_URL: &str = "wss://relay.dannesk.com/relay";
-pub const EXCHANGE_WS_URL: &str = "wss://rates.dannesk.com/rates";
-pub const MAX_RECONNECT_ATTEMPTS: u32 = 10;
-pub const RECONNECT_BACKOFF_SECONDS: u64 = 60;
